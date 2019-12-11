3bf6af83bbe99b02c136a021048d72693d8c85d0
David Jelenc <david.jelenc@fri.uni-lj.si>
2019-12-11 15:33:21 +0100

    Prepares assignment

--- app/src/main/java/ep/ws/MainActivity.kt
+++ app/src/main/java/ep/ws/MainActivity.kt
@@ -1,14 +1,9 @@
 package ep.ws
 
-import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import org.json.JSONObject
-import java.net.HttpURLConnection
-import java.net.URL
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
     companion object {
@@ -23,38 +18,7 @@ class MainActivity : AppCompatActivity() {
 
         search_btn.setOnClickListener {
             Log.i(TAG, "Searching ...")
-            val task = LookUp(this)
-            task.execute(query.text.toString())
             query.setText("")
-        }
-    }
-
-    class LookUp(private val activity: MainActivity) : AsyncTask<String, Unit, JSONObject>() {
-        override fun doInBackground(vararg params: String?): JSONObject {
-            if (params.size != 1)
-                throw IllegalArgumentException("Metoda potrebuje 1 parameter")
-
-            try {
-                val url = URL(String.format(WS_URL, params[0]))
-
-                val conn = (url.openConnection() as HttpURLConnection).apply {
-                    doInput = true
-                    requestMethod = "GET"
-                    setRequestProperty("accept", "application/json")
-                }
-
-                val scanner = Scanner(conn.inputStream).useDelimiter("\\A")
-                val content = if (scanner.hasNext()) scanner.next() else ""
-
-                return JSONObject(content)
-            } catch (e: Exception) {
-                Log.w(TAG, "Exception: ${e.localizedMessage}")
-                return JSONObject()
-            }
-        }
-
-        override fun onPostExecute(result: JSONObject) {
-            activity.results.text = result.toString(2)
         }
     }
 }
