fc29ae34123055c488f5d227852f1f0fd94c09f5
David Jelenc <david.jelenc@fri.uni-lj.si>
2021-12-04 23:03:54 +0100

    Upgrades project

--- app/src/main/java/ep/ws/MainActivity.kt
+++ app/src/main/java/ep/ws/MainActivity.kt
@@ -3,19 +3,20 @@ package ep.ws
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import ep.ws.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val tag = this::class.java.canonicalName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        search_btn.setOnClickListener {
+        binding.searchBtn.setOnClickListener {
             Log.i(tag, "Searching ...")
             // TODO: Klic storitve in prikaz rezultatov
-            query.setText("")
+            binding.query.setText("")
         }
     }
 }
